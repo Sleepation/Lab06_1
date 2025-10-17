84bf5fbe4f70a1e7f4c0dbde3861a0a77e993b89
Thomas Alexandrescu <thomasalexandrescu@gmail.com>
2025-10-17 11:26:23 -0400

    Created SalarySaver Class

---
+++ src/main/java/Lab6One/SalarySaver.java
@@ -0,0 +1,12 @@
+package Lab6One;
+
+import javafx.application.Application;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+public class SalarySaver extends Application {
+    @Override
+    public void start(Stage primaryStage){
+        HBox hBox = new HBox();
+    }
+}
