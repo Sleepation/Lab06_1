c7e126423ac011f59175aa183b4bcdbbac2d8d8a
Thomas Alexandrescu <thomasalexandrescu@gmail.com>
2025-10-17 11:33:03 -0400

    Added UI for SalarySaver

--- src/main/java/Lab6One/SalarySaver.java
+++ src/main/java/Lab6One/SalarySaver.java
@@ -1,12 +1,38 @@
 package Lab6One;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class SalarySaver extends Application {
     @Override
     public void start(Stage primaryStage){
-        HBox hBox = new HBox();
+        VBox vBox = new VBox();
+        vBox.setSpacing(10);
+
+        vBox.getChildren().add(new Text("Enter Salary:"));
+
+        TextField textField = new TextField();
+        vBox.getChildren().add(textField);
+
+        Button btAddSalary = new Button("Add Salary");
+        vBox.getChildren().add(btAddSalary);
+
+        Text numberOfSalariesAdded = new Text();
+
+        Button btDone = new Button("Done");
+
+        Text salaryResult = new Text();
+
+        vBox.getChildren().addAll(numberOfSalariesAdded, btDone, salaryResult);
+
+        primaryStage.setScene(new Scene(vBox, 200, 200));
+        primaryStage.setTitle("Salary Saver");
+        primaryStage.show();
     }
 }
