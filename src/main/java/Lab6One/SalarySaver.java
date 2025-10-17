8576bc136243ffc2b166c94c947afa19f2a83c9f
Thomas Alexandrescu <thomasalexandrescu@gmail.com>
2025-10-17 19:11:39 -0400

    Changed how the salaryResults text is displayed

--- src/main/java/Lab6One/SalarySaver.java
+++ src/main/java/Lab6One/SalarySaver.java
@@ -45,9 +45,13 @@ public class SalarySaver extends Application {
 
         btDone.setOnAction(e -> {
             List<Integer> salaries = FileInteractor.returner(i);
+            salaryResults.setText("Salaries: ");
+            for(int i = 0; i < salaries.size() - 1; i++){
+                salaryResults.setText(salaryResults.getText() + salaries.get(i) + ", ");
+            }
+            salaryResults.setText(salaryResults.getText() + salaries.getLast());
 
-
-            salaryResults.setText("Salaries: " + salaries
+            salaryResults.setText(salaryResults.getText()
             + "\nCount: " + i
             + "\nTotal: " + sum(salaries)
             + "\nAverage: " + average(salaries));
